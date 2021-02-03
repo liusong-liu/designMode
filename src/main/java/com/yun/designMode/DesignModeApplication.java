ef52358a571f036e82aa548be0be113011d7d44a
liusong <1476563920@qq.com>
2021-02-03 17:10:37 +0800

    设计模式

---
+++ src/main/java/com/yun/designMode/DesignModeApplication.java
@@ -0,0 +1,13 @@
+package com.yun.designMode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModeApplication.class, args);
+    }
+
+}
